f129efbf2a0dd68c7308340d4bbe1c2f6fac6d22
Abdul Basit <101786218+engineerbasit@users.noreply.github.com>
2022-03-18 16:31:05 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    my_variable = "This text is passed from flask to HTML"
+    return render_template("HelloHTML.html",my_variable=my_variable)
+
+if __name__ == "__main__":
+    app.run()
